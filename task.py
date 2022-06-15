5a7b0667987b6c5fdde3e2268c6cd46b78e5373e
Supriya090 <khadkasupriya76@gmail.com>
2022-06-15 13:42:54 +0545

    Dynamic Search added

--- task.py
+++ task.py
@@ -1,3 +1,4 @@
+import sys
 import requests
 import sqlite3
 import math
@@ -8,41 +9,60 @@ print(connection.total_changes)
 
 # creating a cursor
 cursor = connection.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS news (page_number INTEGER)")
+cursor.execute(
+    "CREATE TABLE IF NOT EXISTS news (searchTerm STRING, page_number INTEGER)")
 
-# getting news with search term "1"
+
+# getting news with user-specified search term
+searchTerm = input("Enter the term you want to search: ")
 url = "https://bg.annapurnapost.com/api/search"
 params = dict(
-    title='1',
+    title=searchTerm,
 )
 resp = requests.get(url=url, params=params)
 data = resp.json()
 
+
 # getting total number of pages
 total = data['data']['total']
 count = data['data']['count']
+print("Total number of articles of the search term \"",
+      searchTerm, "\" is : ", total)
+if(total == 0):
+    print("No article with the given search term exists")
+    sys.exit()
 pages = math.ceil(total/count)
+
 currentPage = 0
 
-# getting the latest page_number
-value = cursor.execute("SELECT page_number FROM news").fetchall()
-if(len(value) != 0):
-    currentPage = value[-1][0]
+
+# getting the latest page_number based on the search term
+searchRows = cursor.execute(
+    "SELECT searchTerm, page_number FROM news").fetchall()
+if(len(searchRows) != 0):
+    currentTerm = searchRows[-1][0]
+    if(str(currentTerm) != searchTerm):
+        currentPage = 0
+    else:
+        currentPage = searchRows[-1][1]
 
 print(currentPage)
+
+if (currentPage == pages):
+    print("All articles displayed")
 
 
 # pagination
 for i in range(currentPage, pages):
     pageParams = dict(
-        title='1',
+        title=searchTerm,
         page=i+1
     )
     eachResp = requests.get(url=url, params=pageParams)
     eachData = eachResp.json()
     # storing page_number after each response
-    cursor.execute("INSERT INTO news (page_number) VALUES (?)",
-                   (i+1,))
+    cursor.execute("INSERT INTO news (searchTerm, page_number) VALUES (?, ?)",
+                   (searchTerm, i+1,))
     connection.commit()
     print(eachData)
     print("\n\n\n")
